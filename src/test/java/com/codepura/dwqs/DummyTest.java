d1b3f506425018e906afcca6f1e8fdec55566e21
Akshay Pethani <patelaksh412@gmail.com>
2018-07-22 13:56:18 +0530

    Added Dummy Test Cases

---
+++ src/test/java/com/codepura/dwqs/DummyTest.java
@@ -0,0 +1,69 @@
+package com.codepura.dwqs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void dummyOne() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyssOne() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dumsdmyOne() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyOn3e() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyOne5() {
+		assertThat(1).isEqualTo(1);
+	}
+
+
+	@Test
+	public void dummyOned() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyOnes() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyOnesdd() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyOnesd() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyOnew() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyOnedw() {
+		assertThat(1).isEqualTo(1);
+	}
+
+	@Test
+	public void dummyTwo() {
+		assertThat(1).isEqualTo(1);
+	}
+}
